e24f97d1a227d05bda683ecd593e079f238466b4
Pierre BARILE <pbariledev@gmail.com>
2022-07-04 20:58:53 +0200

    signup

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const userCtrl = require('../controllers/user');
 
-router.post('/signup', userCtrl.signup);
-router.post('/login', userCtrl.login);
+router.post('/api/auth/signup', userCtrl.signup);
+router.post('/api/auth/login', userCtrl.login);
 
 module.exports = router;
